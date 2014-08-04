b7e61a357cb2f3e28e350b2e2fbf272616453e17
rama <rama@hammer.com>
2014-08-04 17:12:23 +0930

    Linux update: strcmpi replaced with strcasecmp.

--- code/lunar.cpp
+++ code/lunar.cpp
@@ -120,8 +120,8 @@ int orbiter :: numberOfInputs (void) {return 0;}
 int orbiter :: numberOfOutputs (void) {return 1;}
 char * orbiter :: inputName (int ind) {return "VOID";}
 char * orbiter :: outputName (int ind) {return "SIGNAL";}
-int orbiter :: inputIndex (char * name) {for (int ind = 0; ind < numberOfInputs (); ind++) {if (strcmpi (name, inputName (ind)) == 0) return ind;} return -1;}
-int orbiter :: outputIndex (char * name) {for (int ind = 0; ind < numberOfOutputs (); ind++) {if (strcmpi (name, outputName (ind)) == 0) return ind;} return -1;}
+int orbiter :: inputIndex (char * name) {for (int ind = 0; ind < numberOfInputs (); ind++) {if (strcasecmp (name, inputName (ind)) == 0) return ind;} return -1;}
+int orbiter :: outputIndex (char * name) {for (int ind = 0; ind < numberOfOutputs (); ind++) {if (strcasecmp (name, outputName (ind)) == 0) return ind;} return -1;}
 double orbiter :: output (int ind) {return 0.0;}
 double orbiter :: output (char * name) {int ind = outputIndex (name); if (ind < 0 || ind >= numberOfOutputs ()) return 0.0; return output (ind);}
 void orbiter :: input (double signal, int ind) {}

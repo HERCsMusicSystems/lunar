2f5623b99408e1a387744b83c42a6ee6e0d69ca5
unknown <wolfrob@ADLW0213.corp.knorr-bremse.com>
2015-04-28 15:25:04 +0930

    FM4 Panel rebased onto Base Panel.

--- code/lunar_prolog_eg_panel.cpp
+++ code/lunar_prolog_eg_panel.cpp
@@ -24,23 +24,9 @@
 // This file was created on Wednesday, 6th March 2015 at 09:57:59. //
 /////////////////////////////////////////////////////////////////////
 
-#include "prolog_lunar.h"
-#include "graphic_resources.h"
+#include "lunar_prolog_panel_base.h"
 
-static double prepare (double angle) {
-	angle *= 16384.0;
-	if (angle > 16384.0) angle = 16384.0;
-	if (angle < -16384.0) angle = -16384.0;
-	int ind = (int) angle;
-	return (double) ind;
-}
-static double unprepare (double angle) {
-	if (angle <= -16384.0) return -1.0;
-	if (angle >= 16384.0) return 1.0;
-	return angle / 16384.0;
-}
-
-class eg_panel_action : public PrologNativeCode {
+class eg_panel_action : public AudioModulePanel {
 public:
 	PrologRoot * root;
 	PrologAtom * atom;
@@ -61,14 +47,14 @@ public:
 		PrologAtom * a = 0;
 		double v;
 		switch (ind) {
-		case 0: a = t1; v = prepare (T1 . angle); break;
-		case 1: a = t2; v = prepare (T2 . angle); break;
-		case 2: a = t3; v = prepare (T3 . angle); break;
-		case 3: a = t4; v = prepare (T4 . angle); break;
-		case 4: a = l1; v = prepare (L1 . angle - (feg ? 0.5 : 1.0)); break;
-		case 5: a = l2; v = prepare (L2 . angle - (feg ? 0.5 : 1.0)); break;
-		case 6: a = l3; v = prepare (L3 . angle - (feg ? 0.5 : 1.0)); break;
-		case 7: a = l4; v = prepare (L4 . angle - (feg ? 0.5 : 1.0)); break;
+		case 0: a = t1; v = T1 . value; break;
+		case 1: a = t2; v = T2 . value; break;
+		case 2: a = t3; v = T3 . value; break;
+		case 3: a = t4; v = T4 . value; break;
+		case 4: a = l1; v = L1 . value; break;
+		case 5: a = l2; v = L2 . value; break;
+		case 6: a = l3; v = L3 . value; break;
+		case 7: a = l4; v = L4 . value; break;
 		default: a = 0; break;
 		}
 		if (a == 0) return;
@@ -101,25 +87,48 @@ public:
 			if (! el -> isPair ()) {delete query; return;}
 			el = el -> getLeft (); if (! el -> isPair ()) {delete query; return;}
 			PrologElement * sub = el -> getLeft ();
-			if (sub -> isNumber ()) T1 . angle = unprepare (sub -> getNumber ());
+			if (sub -> isNumber ()) T1 . setValue (sub -> getNumber ());
 			el = el -> getRight (); if (! el -> isPair ()) {delete query; return;} sub = el -> getLeft ();
-			if (sub -> isNumber ()) T2 . angle = unprepare (sub -> getNumber ());
+			if (sub -> isNumber ()) T2 . setValue (sub -> getNumber ());
 			el = el -> getRight (); if (! el -> isPair ()) {delete query; return;} sub = el -> getLeft ();
-			if (sub -> isNumber ()) T3 . angle = unprepare (sub -> getNumber ());
+			if (sub -> isNumber ()) T3 . setValue (sub -> getNumber ());
 			el = el -> getRight (); if (! el -> isPair ()) {delete query; return;} sub = el -> getLeft ();
-			if (sub -> isNumber ()) T4 . angle = unprepare (sub -> getNumber ());
+			if (sub -> isNumber ()) T4 . setValue (sub -> getNumber ());
 			el = el -> getRight (); if (! el -> isPair ()) {delete query; return;} sub = el -> getLeft ();
-			if (sub -> isNumber ()) L1 . angle = unprepare (sub -> getNumber ()) + (feg ? 0.5 : 1.0);
+			if (sub -> isNumber ()) L1 . setValue (sub -> getNumber ());
 			el = el -> getRight (); if (! el -> isPair ()) {delete query; return;} sub = el -> getLeft ();
-			if (sub -> isNumber ()) L2 . angle = unprepare (sub -> getNumber ()) + (feg ? 0.5 : 1.0);
+			if (sub -> isNumber ()) L2 . setValue (sub -> getNumber ());
 			el = el -> getRight (); if (! el -> isPair ()) {delete query; return;} sub = el -> getLeft ();
-			if (sub -> isNumber ()) L3 . angle = unprepare (sub -> getNumber ()) + (feg ? 0.5 : 1.0);
+			if (sub -> isNumber ()) L3 . setValue (sub -> getNumber ());
 			el = el -> getRight (); if (! el -> isPair ()) {delete query; return;} sub = el -> getLeft ();
-			if (sub -> isNumber ()) L4 . angle = unprepare (sub -> getNumber ()) + (feg ? 0.5 : 1.0);
+			if (sub -> isNumber ()) L4 . setValue (sub -> getNumber ());
 		}
 		delete query;
 	}
-	bool code (PrologElement * parameters, PrologResolution * resolution);
+	void redraw (cairo_t * cr) {
+		T1 . draw (cr); T2 . draw (cr); T3 . draw (cr); T4 . draw (cr);
+		L1 . draw (cr); L2 . draw (cr); L3 . draw (cr); L4 . draw (cr);
+	}
+	void MouseKeyon (point location) {
+		T1 . keyon (location); T2 . keyon (location); T3 . keyon (location); T4 . keyon (location);
+		L1 . keyon (location); L2 . keyon (location); L3 . keyon (location); L4 . keyon (location);
+	}
+	void MouseKeyoff (point location) {
+		T1 . keyoff (location); T2 . keyoff (location); T3 . keyoff (location); T4 . keyoff (location);
+		L1 . keyoff (location); L2 . keyoff (location); L3 . keyoff (location); L4 . keyoff (location);
+	}
+	void MouseMove (point delta) {
+		bool redraw = false;
+		if (T1 . move (delta)) {move (0); redraw = true;}
+		if (T2 . move (delta)) {move (1); redraw = true;}
+		if (T3 . move (delta)) {move (2); redraw = true;}
+		if (T4 . move (delta)) {move (3); redraw = true;}
+		if (L1 . move (delta)) {move (4); redraw = true;}
+		if (L2 . move (delta)) {move (5); redraw = true;}
+		if (L3 . move (delta)) {move (6); redraw = true;}
+		if (L4 . move (delta)) {move (7); redraw = true;}
+		if (redraw) update ();
+	}
 	eg_panel_action (GraphicResources * resources, PrologRoot * root, bool feg, PrologAtom * atom,
 		PrologAtom * t1, PrologAtom * t2, PrologAtom * t3, PrologAtom * t4,
 		PrologAtom * l1, PrologAtom * l2, PrologAtom * l3, PrologAtom * l4, bool active) :
@@ -130,13 +139,9 @@ public:
 	L1 (point (18, 98), 0, resources, true, active, feg ? -8192.0 : -16384.0, feg ? 8192.0 : 0.0),
 	L2 (point (88, 98), 0, resources, true, active, feg ? -8192.0 : -16384.0, feg ? 8192.0 : 0.0),
 	L3 (point (158, 98), 0, resources, true, active, feg ? -8192.0 : -16384.0, feg ? 8192.0 : 0.0),
-	L4 (point (228, 98), 0, resources, true, active, feg ? -8192.0 : -16384.0, feg ? 8192.0 : 0.0) {
-		captured_button = 0;
-		background_image = resources != 0 ? resources -> eg_panel_surface : 0;
-		viewport = 0;
+	L4 (point (228, 98), 0, resources, true, active, feg ? -8192.0 : -16384.0, feg ? 8192.0 : 0.0),
+	AudioModulePanel (root, atom, resources != 0 ? resources -> eg_panel_surface : 0) {
 		this -> feg = feg;
-		this -> root = root;
-		this -> atom = atom; COLLECTOR_REFERENCE_INC (atom);
 		this -> t1 = t1; COLLECTOR_REFERENCE_INC (t1);
 		this -> t2 = t2; COLLECTOR_REFERENCE_INC (t2);
 		this -> t3 = t3; COLLECTOR_REFERENCE_INC (t3);
@@ -148,114 +153,10 @@ public:
 		feedback ();
 	}
 	~ eg_panel_action (void) {
-		atom -> setMachine (0);
-		atom -> removeAtom ();
 		t1 -> removeAtom (); t2 -> removeAtom (); t3 -> removeAtom (); t4 -> removeAtom ();
 		l1 -> removeAtom (); l2 -> removeAtom (); l3 -> removeAtom (); l4 -> removeAtom ();
 	}
 };
-
-static gboolean EGPanelDeleteEvent (GtkWidget * viewport, GdkEvent * event, eg_panel_action * machine) {
-	gtk_widget_destroy (machine -> viewport);
-	machine -> remove (false);
-	return FALSE;
-}
-static gboolean RedrawEGPanel (GtkWidget * viewport, GdkEvent * event, eg_panel_action * action) {
-	cairo_t * cr = gdk_cairo_create (gtk_widget_get_window (viewport));
-	if (action -> background_image != 0) {cairo_set_source_surface (cr, action -> background_image, 0, 0); cairo_paint (cr);}
-	action -> T1 . draw (cr);
-	action -> T2 . draw (cr);
-	action -> T3 . draw (cr);
-	action -> T4 . draw (cr);
-	action -> L1 . draw (cr);
-	action -> L2 . draw (cr);
-	action -> L3 . draw (cr);
-	action -> L4 . draw (cr);
-	cairo_destroy (cr);
-	return FALSE;
-}
-static gint EGPanelKeyon (GtkWidget * viewport, GdkEventButton * event, eg_panel_action * action) {
-	action -> captured_button = event -> button;
-	point location (event -> x, event -> y);
-	action -> captured = location;
-	action -> T1 . keyon (location);
-	action -> T2 . keyon (location);
-	action -> T3 . keyon (location);
-	action -> T4 . keyon (location);
-	action -> L1 . keyon (location);
-	action -> L2 . keyon (location);
-	action -> L3 . keyon (location);
-	action -> L4 . keyon (location);
-	return TRUE;
-}
-static gint EGPanelKeyoff (GtkWidget * viewport, GdkEventButton * event, eg_panel_action * action) {
-	point location (event -> x, event -> y);
-	action -> T1 . keyoff (location);
-	action -> T2 . keyoff (location);
-	action -> T3 . keyoff (location);
-	action -> T4 . keyoff (location);
-	action -> L1 . keyoff (location);
-	action -> L2 . keyoff (location);
-	action -> L3 . keyoff (location);
-	action -> L4 . keyoff (location);
-	return TRUE;
-}
-static gint EGPanelMove (GtkWidget * viewport, GdkEventButton * event, eg_panel_action * action) {
-	point location (event -> x, event -> y);
-	point delta = location - action -> captured;
-	if (action -> captured_button > 1) delta *= 0.0078125;
-	action -> captured = location;
-	bool redraw = false;
-	if (action -> T1 . move (delta)) {action -> move (0); redraw = true;}
-	if (action -> T2 . move (delta)) {action -> move (1); redraw = true;}
-	if (action -> T3 . move (delta)) {action -> move (2); redraw = true;}
-	if (action -> T4 . move (delta)) {action -> move (3); redraw = true;}
-	if (action -> L1 . move (delta)) {action -> move (4); redraw = true;}
-	if (action -> L2 . move (delta)) {action -> move (5); redraw = true;}
-	if (action -> L3 . move (delta)) {action -> move (6); redraw = true;}
-	if (action -> L4 . move (delta)) {action -> move (7); redraw = true;}
-	if (redraw) gtk_widget_queue_draw (viewport);
-	return TRUE;
-}
-static gboolean CreateEGPanelIdleCode (eg_panel_action * action) {
-	action -> viewport = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title (GTK_WINDOW (action -> viewport), action -> atom -> name ());
-	g_signal_connect (action -> viewport, "delete-event", G_CALLBACK (EGPanelDeleteEvent), action);
-	GtkWidget * area = gtk_drawing_area_new ();
-	gtk_container_add (GTK_CONTAINER (action -> viewport), area);
-	g_signal_connect (G_OBJECT (area), "expose-event", G_CALLBACK (RedrawEGPanel), action);
-	gtk_widget_add_events (action -> viewport, GDK_BUTTON_PRESS_MASK | GDK_POINTER_MOTION_MASK | GDK_BUTTON_RELEASE_MASK);
-	g_signal_connect (G_OBJECT (action -> viewport), "button_press_event", G_CALLBACK (EGPanelKeyon), action);
-	g_signal_connect (G_OBJECT (action -> viewport), "button_release_event", G_CALLBACK (EGPanelKeyoff), action);
-	g_signal_connect (G_OBJECT (action -> viewport), "motion_notify_event", G_CALLBACK (EGPanelMove), action);
-	if (action -> background_image != 0) gtk_window_resize (GTK_WINDOW (action -> viewport),
-											cairo_image_surface_get_width (action -> background_image),
-											cairo_image_surface_get_height (action -> background_image));
-	gtk_widget_show_all (action -> viewport);
-	return FALSE;
-}
-
-static gboolean RepositionEGPanel (eg_panel_action * action) {
-	gtk_window_move (GTK_WINDOW (action -> viewport), (int) action -> location . x, (int) action -> location . y);
-	return FALSE;
-}
-
-bool eg_panel_action :: code (PrologElement * parameters, PrologResolution * resolution) {
-	if (parameters -> isEarth ()) return remove ();
-	PrologElement * x = 0, * y = 0;
-	PrologElement * refresher = 0;
-	while (parameters -> isPair ()) {
-		PrologElement * el = parameters -> getLeft ();
-		if (el -> isNumber ()) if (x == 0) x = el; else y = el;
-		if (el -> isEarth ()) refresher = el;
-		parameters = parameters -> getRight ();
-	}
-	if (refresher != 0) {feedback (); gtk_widget_queue_draw (viewport); return true;}
-	if (x == 0 || y == 0) return true;
-	location = point (x -> getNumber (), y -> getNumber ());
-	g_idle_add ((GSourceFunc) RepositionEGPanel, this);
-	return true;
-}
 
 bool eg_panel_class :: code (PrologElement * parameters, PrologResolution * resolution) {
 	PrologElement * atom = 0;
@@ -284,7 +185,7 @@ bool eg_panel_class :: code (PrologElement * parameters, PrologResolution * reso
 		t1 -> getAtom (), t2 -> getAtom (), t3 -> getAtom (), t4 -> getAtom (),
 		l1 -> getAtom (), l2 -> getAtom (), l3 -> getAtom (), l4 -> getAtom (), false);
 	if (! atom -> getAtom () -> setMachine (machine)) {delete machine; return false;}
-	g_idle_add ((GSourceFunc) CreateEGPanelIdleCode, machine);
+	machine -> BuildPanel ();
 	return true;
 }
 

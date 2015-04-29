18dd636b7137d0979b1a43586626731adcf27f54
unknown <wolfrob@ADLW0213.corp.knorr-bremse.com>
2015-04-29 12:04:10 +0930

    Parameter Block Panel rebased onto Base Panel.

--- code/lunar_prolog_parameter_block_panel.cpp
+++ code/lunar_prolog_parameter_block_panel.cpp
@@ -24,21 +24,13 @@
 // This file was created on Wednesday, 19th November 2014 at 13:38:23. //
 /////////////////////////////////////////////////////////////////////////
 
-#include "prolog_lunar.h"
-#include "graphic_resources.h"
+#include "lunar_prolog_panel_base.h"
 
-class parameter_block_panel_action : public PrologNativeCode {
+class parameter_block_panel_action : public AudioModulePanel {
 public:
-	PrologRoot * root;
-	PrologAtom * atom;
 	PrologAtom * command;
-	GtkWidget * viewport;
-	point captured;
-	point location;
-	int captured_button;
 	knob_active_graphics ctrl;
-	cairo_surface_t * background_image;
-	void move (double value) {
+	void move (void) {
 		PrologElement * query = root -> pair (root -> atom (command),
 								root -> pair (root -> Double (ctrl . value),
 								root -> earth ()));
@@ -58,104 +50,15 @@ public:
 		}
 		delete query;
 	}
-	bool remove (bool remove_gtk = true) {
-		if (remove_gtk) g_idle_add ((GSourceFunc) RemoveViewportIdleCode, viewport);
-		delete this;
-		return true;
-	}
-
-	bool code (PrologElement * paramters, PrologResolution * resolution);
+	void redraw (cairo_t * cr) {ctrl . draw (cr);}
+	void MouseKeyon (point location) {ctrl . keyon (location);}
+	void MouseKeyoff (point location) {ctrl . keyoff (location);}
+	void MouseMove (point delta) {if (ctrl . move (delta)) {move (); update ();}}
 	parameter_block_panel_action (GraphicResources * resources, PrologRoot * root, PrologDirectory * directory, PrologAtom * atom, PrologAtom * command, bool active) :
-		ctrl (point (0, 0), 0, resources, true, true)
-	{
-		captured_button = 0;
-		background_image = resources != 0 ? resources -> knob_surface : 0;
-		viewport = 0;
-		this -> root = root;
-		this -> atom = atom; COLLECTOR_REFERENCE_INC (atom);
-		this -> command = command; COLLECTOR_REFERENCE_INC (command);
-		feedback ();
-	}
-	~ parameter_block_panel_action (void) {
-		atom -> setMachine (0);
-		atom -> removeAtom ();
-		command -> removeAtom ();
-	}
+		ctrl (point (0, 0), 0, resources, true, true), AudioModulePanel (root, atom, resources != 0 ? resources -> knob_surface : 0)
+	{this -> command = command; COLLECTOR_REFERENCE_INC (command); feedback ();}
+	~ parameter_block_panel_action (void) {command -> removeAtom ();}
 };
-
-static gboolean ParameterBlockPanelDeleteEvent (GtkWidget * viewport, GdkEvent * event, parameter_block_panel_action * machine) {
-	gtk_widget_destroy (machine -> viewport);
-	machine -> remove (false);
-	return FALSE;
-}
-static gboolean RedrawParameterBlockPanel (GtkWidget * viewport, GdkEvent * event, parameter_block_panel_action * action) {
-	cairo_t * cr = gdk_cairo_create (gtk_widget_get_window (viewport));
-	if (action -> background_image != 0) {cairo_set_source_surface (cr, action -> background_image, 0, 0); cairo_paint (cr);}
-	action -> ctrl . draw (cr);
-	cairo_destroy (cr);
-	return FALSE;
-}
-static gint ParameterBlockPanelKeyon (GtkWidget * viewport, GdkEventButton * event, parameter_block_panel_action * action) {
-	action -> captured_button = event -> button;
-	point location (event -> x, event -> y);
-	action -> captured = location;
-	action -> ctrl . keyon (location);
-	return TRUE;
-}
-static gint ParameterBlockPanelKeyoff (GtkWidget * viewport, GdkEventButton * event, parameter_block_panel_action * action) {
-	point location (event -> x, event -> y);
-	action -> ctrl . keyoff (location);
-	return TRUE;
-}
-static gint ParameterBlockPanelMove (GtkWidget * viewport, GdkEventButton * event, parameter_block_panel_action * action) {
-	point location (event -> x, event -> y);
-	point delta = location - action -> captured;
-	if (action -> captured_button > 1) delta *= 0.0078125;
-	action -> captured = location;
-	bool redraw = false;
-	if (action -> ctrl . move (delta)) {action -> move (action -> ctrl . angle); redraw = true;}
-	if (redraw) gtk_widget_queue_draw (viewport);
-	return TRUE;
-}
-static gboolean CreateParameterBlockPanelIdleCode (parameter_block_panel_action * action) {
-	action -> viewport = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title (GTK_WINDOW (action -> viewport), action -> atom -> name ());
-	g_signal_connect (action -> viewport, "delete-event", G_CALLBACK (ParameterBlockPanelDeleteEvent), action);
-	GtkWidget * area = gtk_drawing_area_new ();
-	gtk_container_add (GTK_CONTAINER (action -> viewport), area);
-	g_signal_connect (G_OBJECT (area), "expose-event", G_CALLBACK (RedrawParameterBlockPanel), action);
-	gtk_widget_add_events (action -> viewport, GDK_BUTTON_PRESS_MASK | GDK_POINTER_MOTION_MASK | GDK_BUTTON_RELEASE_MASK);
-	g_signal_connect (G_OBJECT (action -> viewport), "button_press_event", G_CALLBACK (ParameterBlockPanelKeyon), action);
-	g_signal_connect (G_OBJECT (action -> viewport), "button_release_event", G_CALLBACK (ParameterBlockPanelKeyoff), action);
-	g_signal_connect (G_OBJECT (action -> viewport), "motion_notify_event", G_CALLBACK (ParameterBlockPanelMove), action);
-	if (action -> background_image != 0) gtk_window_resize (GTK_WINDOW (action -> viewport),
-											cairo_image_surface_get_width (action -> background_image),
-											cairo_image_surface_get_height (action -> background_image));
-	gtk_widget_show_all (action -> viewport);
-	return FALSE;
-}
-static gboolean RepositionParameterBlockPanel (parameter_block_panel_action * action) {
-	gtk_window_move (GTK_WINDOW (action -> viewport), (int) action -> location . x, (int) action -> location . y);
-	return FALSE;
-}
-
-bool parameter_block_panel_action :: code (PrologElement * parameters, PrologResolution * resolution) {
-	if (parameters -> isEarth ()) return remove ();
-	PrologElement * x = 0;
-	PrologElement * y = 0;
-	PrologElement * refresher = 0;
-	while (parameters -> isPair ()) {
-		PrologElement * el = parameters -> getLeft ();
-		if (el -> isNumber ()) if (x == 0) x = el; else y = el;
-		if (el -> isEarth ()) refresher = el;
-		parameters = parameters -> getRight ();
-	}
-	if (refresher != 0) {feedback (); gtk_widget_queue_draw (viewport); return true;}
-	if (x == 0 || y == 0) return true;
-	location = point (x -> getNumber (), y -> getNumber ());
-	g_idle_add ((GSourceFunc) RepositionParameterBlockPanel, this);
-	return true;
-}
 
 bool parameter_block_panel_class :: code (PrologElement * parameters, PrologResolution * resolution) {
 	PrologElement * atom = 0;
@@ -179,7 +82,7 @@ bool parameter_block_panel_class :: code (PrologElement * parameters, PrologReso
 		if (to != 0) machine -> ctrl . range = to -> getNumber () - machine -> ctrl . start;
 	}
 	if (! atom -> getAtom () -> setMachine (machine)) {delete machine; return false;}
-	g_idle_add ((GSourceFunc) CreateParameterBlockPanelIdleCode, machine);
+	machine -> BuildPanel ();
 	return true;
 }
 

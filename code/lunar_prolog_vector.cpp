015a770155a03ddc1d0bd667ba2c6bfeff8b2f70
unknown <wolfrob@ADLW0213.corp.knorr-bremse.com>
2015-04-29 14:51:34 +0930

    Vector Panel rebased on Base Panel.

--- code/lunar_prolog_vector.cpp
+++ code/lunar_prolog_vector.cpp
@@ -24,22 +24,14 @@
 // This file was created on Friday, 29th August 2014 at 11:45:49 AM. //
 ///////////////////////////////////////////////////////////////////////
 
-#include "prolog_lunar.h"
-#include "graphics2d.h"
-#include "graphic_resources.h"
+#include "lunar_prolog_panel_base.h"
 
-class vector_action : public PrologNativeCode {
+class vector_action : public AudioModulePanel {
 public:
-	PrologRoot * root;
-	PrologAtom * atom;
 	PrologAtom * command;
 	bool on;
-	GtkWidget * viewport;
-	cairo_surface_t * surface;
 	cairo_surface_t * handle;
 	point position;
-	point reference;
-	point screen_position;
 	void action (void) {
 		PrologElement * query = root -> pair (root -> atom (command),
 								root -> pair (root -> Double (1.0 - position . x / 64.0),
@@ -49,117 +41,54 @@ public:
 		root -> resolution (query);
 		delete query;
 	}
-	bool remove (bool remove_gtk = true) {
-		if (remove_gtk) g_idle_add ((GSourceFunc) RemoveViewportIdleCode, viewport);
-		delete this;
+	void feedback (void) {}
+	bool vector_action :: code (PrologElement * parameters, PrologResolution * resolution) {
+		if (parameters -> isEarth ()) return remove ();
+		PrologElement * x = 0;
+		PrologElement * y = 0;
+		while (parameters -> isPair ()) {
+			PrologElement * el = parameters -> getLeft ();
+			if (el -> isNumber ()) if (x == 0) x = el; else y = el;
+			parameters = parameters -> getRight ();
+		}
+		if (x == 0 || y == 0) return true;
+		if (x -> isInteger () && y -> isInteger ()) {
+			location = point (x -> getNumber (), y -> getNumber ());
+			reposition ();
+		}
+		if (x -> isDouble () && y -> isDouble ()) {
+			double xx = x -> getDouble (); if (xx < -1.0) xx = -1.0; if (xx > 1.0) xx = 1.0;
+			double yy = y -> getDouble (); if (yy < -1.0) yy = -1.0; if (yy > 1.0) yy = 1.0;
+			position = point (64.0 - xx * 64, 64.0 + yy * 64.0);
+			update ();
+		}
 		return true;
 	}
-	bool code (PrologElement * parameters, PrologResolution * resolution);
-	vector_action (GraphicResources * resources, PrologRoot * root, PrologAtom * atom, PrologAtom * command) : position (64.0, 64.0) {
+	void redraw (cairo_t * cr) {
+		cairo_surface_t * sub = cairo_surface_create_for_rectangle (handle, position . x, position . y, 129.0, 129.0);
+		cairo_set_source_surface (cr, sub, 23.0, 24.0);
+		cairo_paint (cr);
+		cairo_surface_destroy (sub);
+	}
+	void MouseKeyon (point location) {on = true;}
+	void MouseKeyoff (point location) {on = false;}
+	void MouseMove (point delta) {
+		if (on) {
+			position -= delta * 0.25;
+			if (position . x < 0.0) position . x = 0.0; if (position . y < 0.0) position . y = 0.0;
+			if (position . x > 128.0) position . x = 128.0; if (position . y > 128.0) position . y = 128.0;
+			update ();
+			action ();
+		}
+	}
+	vector_action (GraphicResources * resources, PrologRoot * root, PrologAtom * atom, PrologAtom * command) :
+		AudioModulePanel (root, atom, resources != 0 ? resources -> vector_surface : 0), position (64.0, 64.0) {
 		on = false;
-		this -> root = root;
-		this -> atom = atom; COLLECTOR_REFERENCE_INC (atom);
 		this -> command = command; COLLECTOR_REFERENCE_INC (command);
-		viewport = 0;
-		this -> surface = resources -> vector_surface;
 		this -> handle = resources -> vector_handle;
 	}
-	~ vector_action (void) {
-		atom -> setMachine (0);
-		atom -> removeAtom ();
-		command -> removeAtom ();
-	}
+	~ vector_action (void) {command -> removeAtom ();}
 };
-
-static gboolean RepositionVector (vector_action * vector) {
-	gtk_window_move (GTK_WINDOW (vector -> viewport), (int) vector -> screen_position . x, (int) vector -> screen_position . y);
-	return FALSE;
-}
-static gboolean RepaintVector (vector_action * vector) {gtk_widget_queue_draw (vector -> viewport); return FALSE;}
-
-bool vector_action :: code (PrologElement * parameters, PrologResolution * resolution) {
-	if (parameters -> isEarth ()) return remove ();
-	PrologElement * x = 0;
-	PrologElement * y = 0;
-	while (parameters -> isPair ()) {
-		PrologElement * el = parameters -> getLeft ();
-		if (el -> isNumber ()) if (x == 0) x = el; else y = el;
-		parameters = parameters -> getRight ();
-	}
-	if (x == 0 || y == 0) return true;
-	if (x -> isInteger () && y -> isInteger ()) {
-		screen_position = point (x -> getNumber (), y -> getNumber ());
-		g_idle_add ((GSourceFunc) RepositionVector, this);
-	}
-	if (x -> isDouble () && y -> isDouble ()) {
-		double xx = x -> getDouble (); if (xx < -1.0) xx = -1.0; if (xx > 1.0) xx = 1.0;
-		double yy = y -> getDouble (); if (yy < -1.0) yy = -1.0; if (yy > 1.0) yy = 1.0;
-		position = point (64.0 - xx * 64, 64.0 + yy * 64.0);
-		g_idle_add ((GSourceFunc) RepaintVector, this);
-	}
-	return true;
-}
-
-static gboolean ViewportDeleteEvent (GtkWidget * viewport, GdkEvent * event, vector_action * machine) {
-	gtk_widget_destroy (machine -> viewport);
-	machine -> remove (false);
-	return FALSE;
-}
-
-static gboolean RedrawVector (GtkWidget * viewport, GdkEvent * event, vector_action * machine) {
-	cairo_t * cr = gdk_cairo_create (gtk_widget_get_window (viewport));
-	if (machine -> surface == 0) return FALSE;
-	cairo_set_source_surface (cr, machine -> surface, 0.0, 0.0);
-	cairo_paint (cr);
-	cairo_surface_t * sub = cairo_surface_create_for_rectangle (machine -> handle, machine -> position . x, machine -> position . y, 129.0, 129.0);
-	cairo_set_source_surface (cr, sub, 23.0, 24.0);
-	cairo_paint (cr);
-	cairo_surface_destroy (sub);
-	cairo_destroy (cr);
-	return FALSE;
-}
-
-static gint VectorKeyon (GtkWidget * viewport, GdkEventButton * event, vector_action * machine) {
-	machine -> on = true;
-	machine -> reference = point (event -> x, event -> y);
-	return TRUE;
-}
-
-static gint VectorKeyoff (GtkWidget * viewport, GdkEventButton * event, vector_action * machine) {
-	machine -> on = false;
-	return TRUE;
-}
-
-static gint VectorMove (GtkWidget * viewport, GdkEventButton * event, vector_action * machine) {
-	if (machine -> on) {
-		point p (event -> x, event -> y);
-		machine -> position -= (p - machine -> reference) * 0.25;
-		machine -> reference = p;
-		if (machine -> position . x < 0.0) machine -> position . x = 0.0;
-		if (machine -> position . y < 0.0) machine -> position . y = 0.0;
-		if (machine -> position . x > 128.0) machine -> position . x = 128.0;
-		if (machine -> position . y > 128.0) machine -> position . y = 128.0;
-		gtk_widget_queue_draw (machine -> viewport);
-		machine -> action ();
-	}
-	return TRUE;
-}
-
-static gboolean CreateVectorIdleCode (vector_action * parameter) {
-	parameter -> viewport = gtk_window_new (GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title (GTK_WINDOW (parameter -> viewport), parameter -> atom -> name ());
-	g_signal_connect (parameter -> viewport, "delete-event", G_CALLBACK (ViewportDeleteEvent), parameter);
-	GtkWidget * area = gtk_drawing_area_new ();
-	gtk_container_add (GTK_CONTAINER (parameter -> viewport), area);
-	g_signal_connect (G_OBJECT (area), "expose-event", G_CALLBACK (RedrawVector), parameter);
-	gtk_widget_add_events (parameter -> viewport, GDK_BUTTON_PRESS_MASK | GDK_POINTER_MOTION_MASK | GDK_BUTTON_RELEASE_MASK);
-	g_signal_connect (G_OBJECT (parameter -> viewport), "button_press_event", G_CALLBACK (VectorKeyon), parameter);
-	g_signal_connect (G_OBJECT (parameter -> viewport), "button_release_event", G_CALLBACK (VectorKeyoff), parameter);
-	g_signal_connect (G_OBJECT (parameter -> viewport), "motion_notify_event", G_CALLBACK (VectorMove), parameter);
-	gtk_window_resize (GTK_WINDOW (parameter -> viewport), cairo_image_surface_get_width (parameter -> surface), cairo_image_surface_get_height (parameter -> surface));
-	gtk_widget_show_all (parameter -> viewport);
-	return FALSE;
-}
 
 bool vector_class :: code (PrologElement * parameters, PrologResolution * resolution) {
 	PrologElement * atom = 0;
@@ -176,7 +105,7 @@ bool vector_class :: code (PrologElement * parameters, PrologResolution * resolu
 	if (atom -> getAtom () -> getMachine () != 0) return false;
 	vector_action * machine = new vector_action (resources, root, atom -> getAtom (), command -> getAtom ());
 	if (! atom -> getAtom () -> setMachine (machine)) {delete machine; return false;}
-	g_idle_add ((GSourceFunc) CreateVectorIdleCode, machine);
+	machine -> BuildPanel ();
 	return true;
 }
 

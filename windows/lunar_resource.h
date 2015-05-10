a93068768f639481fa2840065b9e2199c3b1d621
unknown <wolfrob@ADLW0213.corp.knorr-bremse.com>
2015-05-11 08:12:37 +0930

    Windows update.

--- windows/lunar_resource.h
+++ windows/lunar_resource.h
@@ -34,6 +34,7 @@
 #define FILTER_PANEL_PNG                227
 #define DELAY_PANEL_PNG                 228
 #define CHORUS_PANEL_PNG                229
+#define FREEVERB_PANEL_PNG              230
 
 #define SMALL_KEYBOARD_SIZE             435
 #define KEYBOARD_SIZE                   579
@@ -63,10 +64,11 @@
 #define FILTER_PANEL_SIZE               16548
 #define DELAY_PANEL_SIZE                16867
 #define CHORUS_PANEL_SIZE               19026
+#define FREEVERB_PANEL_SIZE             21688
 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        230
+#define _APS_NEXT_RESOURCE_VALUE        231
 #define _APS_NEXT_COMMAND_VALUE         40001
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101

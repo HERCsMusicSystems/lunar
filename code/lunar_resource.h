23f531b212aa036500ddad0b01a2e587b6e349e6
robert <robert@haesemathematics.com.au>
2015-08-11 15:02:23 +0930

    Resources added.

---
+++ code/lunar_resource.h
@@ -0,0 +1,78 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by lunar.rc
+
+// Next default values for new objects
+// 
+
+#define LUNAR_PRC                       201
+#define SMALL_KEYBOARD_PNG              202
+#define KEYBOARD_PNG                    203
+#define BIG_KEYBOARD_PNG                204
+#define VECTOR_FRAME_PNG                205
+#define VECTOR_HANDLE_PNG				206
+#define KNOB_PNG                        207
+#define KNOB_SURFACE_PNG                208
+#define KNOB_HANDLE_PNG                 209
+#define DISPLAY_PNG                     210
+#define BUTTON_ON_PNG                   211
+#define BUTTON_OFF_PNG                  212
+#define ENCODER_SURFACE_PNG             213
+#define ENCODER_HANDLE_PNG              214
+#define SLIDER_SURFACE_PNG              215
+#define SLIDER_HANDLE_PNG               216
+#define COMMAND_CENTRE_PNG              217
+#define ADSR_PANEL_PNG                  218
+#define EG_PANEL_PNG                    219
+#define FM4_PANEL_PNG                   220
+#define CORE_PANEL_PNG                  221
+#define LFO_PANEL_PNG                   222
+#define LFO_VIBRATO_PNG                 223
+#define LFO_TREMOLO_PNG                 224
+#define LFO_WAH_WAH_PNG                 225
+#define LFO_PAN_PNG                     226
+#define FILTER_PANEL_PNG                227
+#define DELAY_PANEL_PNG                 228
+#define CHORUS_PANEL_PNG                229
+#define STEREO_CHORUS_PANEL_PNG         230
+#define FREEVERB_PANEL_PNG              231
+
+#define SMALL_KEYBOARD_SIZE             435
+#define KEYBOARD_SIZE                   579
+#define BIG_KEYBOARD_SIZE               1765
+#define VECTOR_FRAME_SIZE               6171
+#define VECTOR_HANDLE_SIZE              1151
+#define KNOB_SIZE                       3917
+#define KNOB_SURFACE_SIZE               3840
+#define KNOB_HANDLE_SIZE                244
+#define DISPLAY_SIZE                    231063
+#define BUTTON_ON_SIZE                  231063
+#define BUTTON_OFF_SIZE                 213063
+#define ENCODER_SURFACE_SIZE            213063
+#define ENCODER_HANDLE_SIZE             213063
+#define SLIDER_SURFACE_SIZE             213063
+#define SLIDER_HANDLE_SIZE              213063
+#define COMMAND_CENTRE_SIZE             213063
+#define ADSR_PANEL_SIZE                 19916
+#define EG_PANEL_SIZE                   37224
+#define FM4_PANEL_SIZE                  66525
+#define CORE_PANEL_SIZE                 114617
+#define LFO_PANEL_SIZE                  48642
+#define LFO_VIBRATO_SIZE                5826
+#define LFO_TREMOLO_SIZE                5763
+#define LFO_WAH_WAH_SIZE                5847
+#define LFO_PAN_SIZE                    5529
+#define FILTER_PANEL_SIZE               16548
+#define DELAY_PANEL_SIZE                16867
+#define CHORUS_PANEL_SIZE               19026
+#define STEREO_CHORUS_PANEL_SIZE        24334
+#define FREEVERB_PANEL_SIZE             21688
+
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        232
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
